fcae7c8076c3fb7e433d0f9dc069fbcf4f730694
Your Name <you@example.com>
2023-11-01 16:45:53 +0100

    Udělání další sekce a scroll na tlačítku pojďme na to, zítra vložit na stránku zbytek textů a obrázků v druhé a třetí sekci

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,5 +1,10 @@
 //neriskujeme že se projekt nenačte
 $(document).ready(function(){
+
+//scrolování na nadpis "Už vás nebaví psát si zápisky jako prostý text zkuste myšlenkové mapy"
+$(".header-wrap__button").click(function(){
+    $("html, body").animate({scrollTop: $("#Explain").offset().top},1000);
+});
 //první slider na webu s textem "O myšlenkové mapě"
 $('.explain-text__slider').slick({
     slidesToShow: 1,
@@ -7,7 +12,6 @@ $('.explain-text__slider').slick({
     prevArrow: $('.explain-text__arrowLeft'),
     nextArrow: $('.explain-text__arrowRight')
 });
-
 //druhý slider na webu s obrázkama v sekci "O myšlenkové mapě"
 $('.explain-image__slider').slick({
     slidesToShow: 1,
@@ -15,4 +19,12 @@ $('.explain-image__slider').slick({
     dots: true,
     arrows: false
 });
+//třetí slider na webu s textem a obrázkem v sekci "Nabídka"
+$('.offer-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.offer-slider__arrowLeft'),
+    nextArrow: $('.offer-slider__arrowRight')
+});
+
 });
