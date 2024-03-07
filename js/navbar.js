57b0f6af20782d553c93e8f6ad25836b4e89cec5
Your Name <you@example.com>
2024-03-07 22:13:27 +0100

    jeste zkontrolovat hlavni html soubor a pak natoči video a je to hotovy

--- js/navbar.js
+++ js/navbar.js
@@ -1,70 +1,70 @@
 $(document).ready(function () {
   
   function smoothScroll(target) {
-    $("html, body").animate({ scrollTop: $(target).offset().top }, 1200);
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 1200);
   }
-// Scrolování na sekci O myšlenkových mapách
-$("#Aexplain").click(function () {
-    window.location.href.indexOf("index.html") !== -1 ? smoothScroll("#explain") : window.location.href = "index.html";
-    $(".hamburger").removeClass('is-active')
-    $(".navbar-links").removeClass('mobile-nav')
-    $("body").removeClass('body-overflowY')
+
+  // Scrolování na sekci O myšlenkových mapách
+  $('#Aexplain').click(function () {
+    window.location.href.indexOf('index.html') !== -1 ? smoothScroll('#explain') : window.location.href = 'index.html';
+    $('.hamburger').removeClass('is-active');
+    $('.navbar-links').removeClass('mobile-nav');
+    $('body').removeClass('body-overflowY');
   });
   
   // Scrolování na sekci Nástroje
-  $("#Atools").click(function () {
-    window.location.href.indexOf("index.html") !== -1 ? smoothScroll("#tools") : window.location.href = "index.html";
-    $(".hamburger").removeClass('is-active')
-    $(".navbar-links").removeClass('mobile-nav')
-    $("body").removeClass('body-overflowY')
+  $('#Atools').click(function () {
+    window.location.href.indexOf('index.html') !== -1 ? smoothScroll('#tools') : window.location.href = 'index.html';
+    $('.hamburger').removeClass('is-active');
+    $('.navbar-links').removeClass('mobile-nav');
+    $('body').removeClass('body-overflowY');
   });
 
   //Přesměrování na sekci O Mně
-  $("#Amyself").click(function () {
-    window.location.href.indexOf("myself.html") !== -1 ? smoothScroll("#myself") : window.location.href = "myself.html";
-    $(".hamburger").removeClass('is-active')
-    $(".navbar-links").removeClass('mobile-nav')
-    $("body").removeClass('body-overflowY')
+  $('#Amyself').click(function () {
+    window.location.href.indexOf('myself.html') !== -1 ? smoothScroll('#myself') : window.location.href = 'myself.html';
+    $('.hamburger').removeClass('is-active');
+    $('.navbar-links').removeClass('mobile-nav');
+    $('body').removeClass('body-overflowY');
   });
 
   //Přesměrování na sekci Moje tvorba
-  $("#Amyprojects").click(function () {
-    window.location.href.indexOf("myself.html") !== -1 ? smoothScroll("#myprojects") : window.location = "myself.html";
-    $(".hamburger").removeClass('is-active')
-    $(".navbar-links").removeClass('mobile-nav')
-    $("body").removeClass('body-overflowY')
-});
+  $('#Amyprojects').click(function () {
+    window.location.href.indexOf('myself.html') !== -1 ? smoothScroll('#myprojects') : window.location = 'myself.html';
+    $('.hamburger').removeClass('is-active');
+    $('.navbar-links').removeClass('mobile-nav');
+    $('body').removeClass('body-overflowY');
+  });
 
   // Scrolování na sekci Kontakt
-  $("#Afooter").click(function () {
-    smoothScroll("#footer");
-    $(".hamburger").removeClass('is-active')
-    $(".navbar-links").removeClass('mobile-nav')
-    $("body").removeClass('body-overflowY')
+  $('#Afooter').click(function () {
+    smoothScroll('#footer');
+    $('.hamburger').removeClass('is-active');
+    $('.navbar-links').removeClass('mobile-nav');
+    $('body').removeClass('body-overflowY');
   });
  
-  //navbar v mobilní verzi
-  $(".hamburger").click(function(){
-    $(".hamburger").toggleClass('is-active')
-    $(".navbar-links").toggleClass('mobile-nav')
+  // navigace v mobilní verzi
+  $('.hamburger').click(function(){
+    $('.hamburger').toggleClass('is-active');
+    $('.navbar-links').toggleClass('mobile-nav');
+    //změna ikonek rozdílné barvy
+    var Mobile_navbar_SRC_ig = ($('#ig-nav').attr('src') === './img/instagram-icon.svg') ? './img/instagram-icon2.svg' : './img/instagram-icon.svg';
+    $('#ig-nav').attr('src', Mobile_navbar_SRC_ig);
   
-    var newSrcig = ($('#ig-nav').attr('src') === './img/instagram-icon.svg') ? './img/instagram-icon2.svg' : './img/instagram-icon.svg';
-    $('#ig-nav').attr('src', newSrcig);
-  
-    var newSrcf = ($('#f-nav').attr('src') === './img/facebook-icon.svg') ? './img/facebook-icon2.svg' : './img/facebook-icon.svg';
-    $('#f-nav').attr('src', newSrcf);
-  
-    $("body").toggleClass('body-overflowY')
-    })
-  
-  
-  $("#ig-nav").hover(function(){
-    var newHoverig = ($('#ig-nav').attr('src') === './img/instagram-icon.svg') ? './img/instagram-icon2.svg' : './img/instagram-icon.svg'
-    $('#ig-nav').attr('src', newHoverig)
+    var Mobile_navbar_SRC_f = ($('#f-nav').attr('src') === './img/facebook-icon.svg') ? './img/facebook-icon2.svg' : './img/facebook-icon.svg';
+    $('#f-nav').attr('src', Mobile_navbar_SRC_f);
+    //aby zmizela scrollovací lišta
+    $('body').toggleClass('body-overflowY');
   });
   
-  $("#f-nav").hover(function(){
-    var newHoverf = ($('#f-nav').attr('src') === './img/facebook-icon.svg') ? './img/facebook-icon2.svg' : './img/facebook-icon.svg'
-    $('#f-nav').attr('src', newHoverf)
+  $('#ig-nav').hover(function(){
+    var Desktop_navbar_SRC_ig = ($('#ig-nav').attr('src') === './img/instagram-icon.svg') ? './img/instagram-icon2.svg' : './img/instagram-icon.svg';
+    $('#ig-nav').attr('src', Desktop_navbar_SRC_ig);
+  });
+  
+  $('#f-nav').hover(function(){
+    var Desktop_navbar_SRC_f = ($('#f-nav').attr('src') === './img/facebook-icon.svg') ? './img/facebook-icon2.svg' : './img/facebook-icon.svg';
+    $('#f-nav').attr('src', Desktop_navbar_SRC_f);
     });
 });
