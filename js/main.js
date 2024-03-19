869a90c1c993c7350c93504bc4804f580fd608d4
Your Name <you@example.com>
2024-03-19 16:58:36 +0100

    2

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//toto nám zajistí, že se kód načte až po načtení stránky, abz nedošlo, že nějaké prvky nenajde
+$(document).ready(function () {
+ // Zobrazení navigace a formuláře na hlavní stránce
+  $('#navbar').load('navbar.html');
+  $('#footer').load('footer.html');
+
+  //definování funkce scrollování na danou sekci, target = cílový element, offset je že to dojede úplně nahoru toho elementu
+  function smoothScroll(target) {
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 1200);
+  }
+  //definování funkce k zobrazení
+  function openModal(modalId) {
+    $(modalId).addClass('modal-active');
+    $('body').addClass('body-overflowY');
+  }
+  //definování funkce k zavření obrázků
+  function closeModal(modalId) {
+    $(modalId).removeClass('modal-active');
+    $('body').removeClass('body-overflowY');
+  }
+  
+  // Scrolování na sekci O myšlenkových mapách
+  $('#explainButton').click(function () {
+    smoothScroll('#explain');
+  });
+  
+  // Scrolování na sekci O mně
+  $('#myselfButton').click(function () {
+    smoothScroll('#myself');
+  });
+
+  // Slidery
+  $('#explainSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '.explain-slider__arrowLeft',
+    nextArrow: '.explain-slider__arrowRight'
+  });
+
+  $('#toolsSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true
+  });
+  
+  // Modální okna
+  for (let i = 1; i <= 6; i++) {
+    $(`#explainWiden_${i}`).click(function () {
+      openModal(`#explainModal_${i}`);
+    });
+
+    $(`#explainClose_${i}`).click(function () {
+      closeModal(`#explainModal_${i}`);
+    });
+  }
+  
+  // Modální okna O Mně
+  for (let i = 1; i <= 4; i++) {
+    $(`#myProjectsWiden_${i}`).click(function () {
+      openModal(`#myProjectsModal_${i}`);
+    });
+  
+    $(`#myProjectsClose_${i}`).click(function () {
+      closeModal(`#myProjectsModal_${i}`);
+    });
+  }
+});
